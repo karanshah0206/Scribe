8b21e90e845ee130c863e71fd3d5d4dbceff8bcb
Karan Shah <karanshah0206@outlook.com>
2020-10-01 14:40:03 +0530

    Signup UI Created

--- Scribe/src/app/app.module.ts
+++ Scribe/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
