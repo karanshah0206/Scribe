7a82680a646f7cf24488c157b6d329d955ac75a1
Karan Shah <karanshah0206@outlook.com>
2020-10-01 15:01:39 +0530

    Bound Form Values to TS Reactive Form + Validators

--- Scribe/src/app/app.module.ts
+++ Scribe/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -12,7 +13,8 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Scribe/src/app/signup/signup.component.ts
+++ Scribe/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) {
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      verPass: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  signUpHandle(form) {
+    console.log(form.value);
   }
 
 }
