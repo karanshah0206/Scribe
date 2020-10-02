aa894b9c5e35601f5c09464e12d9af880d4b2c23
Karan Shah <karanshah0206@outlook.com>
2020-10-02 15:23:58 +0530

    Reactive Login Form Established

--- Scribe/src/app/login/login.component.ts
+++ Scribe/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  loginHandle(form) {
+    console.log(form.value);
   }
 
 }
